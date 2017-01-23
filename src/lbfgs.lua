18055dedf1c3653041827bc246054aca0d601291
Nick Papior <nickpapior@gmail.com>
2017-01-23 22:42:32 +0100

    maint: updated variable names (WIKI)
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- src/lbfgs.lua
+++ src/lbfgs.lua
@@ -1,7 +1,7 @@
-
 --[[ 
-Create a table with the default parameters of 
-the Optima functions that are going to be inherited.
+This module implements the L-BFGS algorithm
+for minimization of a functional with an accompanying
+gradient.
 --]]
 
 local m = require "math"
@@ -20,7 +20,7 @@ function LBFGS:initialize(...)
    --    over-relaxed
    --  damp < 1
    --    under-relaxed
-   self.damp = 1.
+   self.damp = 1.1
    
    -- Initial inverse Hessian
    -- Lower values converges faster at the risk of
@@ -34,28 +34,27 @@ function LBFGS:initialize(...)
    -- Currently reached itteration
    self.nitt = 0
 
-   -- Constraint variable to minimize
-   -- conv_C is the convergence tolerance of the constraint
-   self.tolerance_C = 0.002
+   -- this is the convergence tolerance of the gradient
+   self.tolerance = 0.02
    self.is_optimized = false
    
    -- Maximum change in functional allowed (cut-off)
-   self.max_dP = 0.1
+   self.max_dF = 0.1
    
    -- Field of the functional we wish to optimize
    --
-   --   P == optimization variable/functional
-   --   C == constraint variable/functional (minimization)
-   self.P0 = {}
-   self.C0 = {}
+   --   F == optimization variable/functional
+   --   G == gradient variable/functional (minimization)
+   self.F0 = {}
+   self.G0 = {}
 
    -- History fields of the residuals.
    -- We store the residuals of the
-   --   dP == optimization variable/functional
-   --   dC == constraint variable/functional (minimization)
+   --   dF == optimization variable/functional
+   --   dG == gradient variable/functional (minimization)
    --   rho is the kernel of the residual dot-product
-   self.dP = {}
-   self.dC = {}
+   self.dF = {}
+   self.dG = {}
    self.rho = {}
 
    -- Ensure we update the elements as passed
@@ -75,10 +74,10 @@ end
 -- Correct the step-size (change of optimization variable)
 -- by asserting that the norm of each vector is below
 -- a given threshold.
-function LBFGS:correct_dP (dP)
+function LBFGS:correct_dF (dF)
 
    -- Calculate the norm for each field
-   local norm = self.norm1D(dP)
+   local norm = self.norm1D(dF)
 
    -- Calculate each elements maximum norm
    local max_norm = 0.
@@ -86,21 +85,21 @@ function LBFGS:correct_dP (dP)
       max_norm = m.max(max_norm, norm[i])
    end
    
-   -- Now normalize dP
-   norm = self.max_dP / max_norm
+   -- Now normalize the displacement
+   norm = self.max_dF / max_norm
    if norm < 1. then
-      return dP * norm
+      return dF * norm
    else
-      return dP
+      return dF
    end
    
 end
 
 -- Add the current optimization variable and the
--- constraint variable to the history.
+-- gradient variable to the history.
 -- This function calculates the residuals
 -- and updates the kernel of the residual dot-product.
-function LBFGS:add_history (P, C)
+function LBFGS:add_history (F, G)
 
    -- Retrieve the current itteration step.
    -- With respect to the history and total
@@ -109,14 +108,14 @@ function LBFGS:add_history (P, C)
 
    -- If the current itteration count is
    -- more than or equal to one, it means that
-   -- we already have P0 and C0
+   -- we already have F0 and G0
    if itt > 0 then
 
-      self.dP[itt] = P - self.P0
-      self.dC[itt] = C - self.C0
+      self.dF[itt] = F - self.F0
+      self.dG[itt] = G - self.G0
       
       -- Calculate dot-product and store the kernel
-      self.rho[itt] = 1. / self.dP[itt]:reshape(-1):dot(self.dC[itt]:reshape(-1))
+      self.rho[itt] = 1. / self.dF[itt]:reshape(-1):dot(self.dG[itt]:reshape(-1))
       
    end
    
@@ -125,43 +124,43 @@ function LBFGS:add_history (P, C)
    if itt > self.history then
 
       -- Forcing garbage collection
-      table.remove(self.dP, 1)
-      table.remove(self.dC, 1)
+      table.remove(self.dF, 1)
+      table.remove(self.dG, 1)
       table.remove(self.rho, 1)
 	 
    end
 
    -- Ensure that the next itteration has
    -- the input sequence
-   self.P0 = P:copy()
-   self.C0 = C:copy()
+   self.F0 = F:copy()
+   self.G0 = G:copy()
 
 end
 
--- Calculate the optimized variable (P) which
--- minimizes the constraint (C).
-function LBFGS:next (P, C)
+-- Calculate the optimized variable (F) which
+-- minimizes the gradient (G).
+function LBFGS:next (F, G)
    
    -- Add the current iteration to the history
-   self:add_history(P, C)
+   self:add_history(F, G)
 
    -- Retrieve current itteration count
    local itt = self:itt()
 
    -- Create local pointers to tables
    -- (they are tables, hence by-reference)
-   local dP = self.dP
-   local dC = self.dC
+   local dF = self.dF
+   local dG = self.dG
    local rho = self.rho
 
    -- Create table for accumulating dot products
    local rh = {}
    
-   -- Create a copy of the constraint
-   local q = C:reshape(-1)
+   -- Update the uphill gradient
+   local q = G:reshape(-1)
    for i = itt, 1, -1 do
-      rh[i] = rho[i] * dP[i]:reshape(-1):dot(q)
-      q = q - rh[i] * dC[i]:reshape(-1)
+      rh[i] = rho[i] * dF[i]:reshape(-1):dot(q)
+      q = q - rh[i] * dG[i]:reshape(-1)
    end
 
    -- Solve for the rhs optimization
@@ -171,43 +170,43 @@ function LBFGS:next (P, C)
 
    -- Now create the next step
    for i = 1, itt do
-      local beta = rho[i] * dC[i]:reshape(-1):dot(z)
-      z = z + dP[i]:reshape(-1) * (rh[i] - beta)
+      local beta = rho[i] * dG[i]:reshape(-1):dot(z)
+      z = z + dF[i]:reshape(-1) * (rh[i] - beta)
    end
    
    -- Ensure shape
-   local p = - z:reshape(C.size)
+   z = - z:reshape(G.size)
    
    -- Update step
-   local dp = self:correct_dP(p) * self.damp
+   local delta = self:correct_dF(z) * self.damp
    
    -- Determine whether we have optimized the parameter/functional
-   self:optimized(C)
+   self:optimized(G)
    
    -- Calculate next step
-   local newP
+   local newF
    if not self.is_optimized then
-      newP = P + dp
+      newF = F + delta
    else
-      newP = P:copy()
+      newF = F:copy()
    end
 
    self.nitt = self.nitt + 1
    
-   return newP
+   return newF
       
 end
 
 -- Function to determine whether the
 -- LBFGS algorithm has converged
-function LBFGS:optimized (C)
+function LBFGS:optimized (G)
    -- Check convergence
-   local norm = self.norm1D(C)
+   local norm = self.norm1D(G)
 
    -- Determine whether the algorithm is complete.
    self.is_optimized = true
    for i = 1, #norm do
-      if norm[i] > self.tolerance_C then
+      if norm[i] > self.tolerance then
 	 self.is_optimized = false
       end
    end
@@ -224,8 +223,8 @@ function LBFGS:info ()
    print("LBGFS current / history: "..tostring(self:itt()) .. " / "..self.history)
    print("LBGFS.damping "..tostring(self.damp))
    print("LBGFS.H0 "..tostring(self.H0))
-   print("LBGFS.Tolerance "..tostring(self.tolerance_C))
-   print("LBGFS.Max-dP "..tostring(self.max_dP))
+   print("LBGFS.Tolerance "..tostring(self.tolerance))
+   print("LBGFS.max-dF "..tostring(self.max_dF))
 
 
 end
