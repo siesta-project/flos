ce77df66276f17e7a9ab23771bafc2b4158e5dd3
arsalan-akhtar <arsalan_akhtar@outlook.com>
2019-03-06 11:58:29 +0100

    VC-NEB added

--- flos/special/neb.lua
+++ flos/special/neb.lua
@@ -73,25 +73,28 @@ local NEB = mc.class("NEB")
 -- @number[opt=0.005] climbing_tol the tolerance for determining whether an image is climbing or not
 local function doc_function()
 end
-
-
 -- Initialization routine
-function NEB:initialize(images, tbl)
+function NEB:initialize(images,tbl)
    -- Convert the remaining arguments to a table
    local tbl = tbl or {}
-
-   -- Copy all images over
+   --local vectors={}
+   --local vc_image={}
+   --self.vectors=vectors
+   --self.vc_image=images
+   --self.vc_image=images[1]
+   --self.vc_image.insert(1)
+   --for k,v in pairs(vectors) do table.insert(self.vc_image, v) end
+     -- Copy all images over
    local size_img = #images[1].R
    for i = 1, #images do
       self[i-1] = images[i]
+      self.zeros=images[i].R- images[i].R
       if #images[i].R ~= size_img then
 	 error("NEB: images does not have same size of geometries!")
       end
    end
-
    -- store the number of images (without the initial and final)
    self.n_images = #images - 2
-
    -- This is _bad_ practice, however,
    -- the middleclass system does not easily enable overwriting
    -- the __index function (because it uses it)
@@ -101,8 +104,7 @@ function NEB:initialize(images, tbl)
    self.DM_label="MgO-3x3x1-2V"
    --self.old_DM_label=""
    --self.current_DM_label=""
-   
-   -- an integer that describes when the climbing image
+      -- an integer that describes when the climbing image
    -- may be used, make large enough to never set it
    local cl = tbl.climbing or 5
    if cl == false then
@@ -114,12 +116,9 @@ function NEB:initialize(images, tbl)
       -- Counter for climbing
       self._climbing = cl
    end
-
    -- Set the climbing energy tolerance
    self.climbing_tol = tbl.climbing_tol or 0.005 -- if the input is in eV/Ang this is 5 meV
-
    self.niter = 0
-
    -- One should also attach the spring-constant
    -- It currently defaults to 5
    local kl = tbl.k or 5.
@@ -133,24 +132,26 @@ function NEB:initialize(images, tbl)
 			       end
 			    })
    end
-   
-   self:init_files()
-   
-   
-   
+   self:init_files()   
 end
 
 -- Simple wrapper for checking the image number
-function NEB:_check_image(image, all)
+function NEB:_check_image(image,all)
    local all = all or false
    if all then
       if image < 0 or self.n_images + 1 < image then
 	 error("NEB: requesting a non-existing image!")
       end
+    --  if image_vector < 0 or self.n_images + 1 < image_vector then
+	 --error("NEB: requesting a non-existing image!")
+   --   end
    else
       if image < 1 or self.n_images < image then
 	 error("NEB: requesting a non-existing image!")
       end
+    --  if image_vector < 1 or self.n_images < image_vector then
+	 --error("NEB: requesting a non-existing image!")
+    --  end
    end
 end
 
@@ -162,12 +163,9 @@ end
 function NEB:dR(img1, img2)
    self:_check_image(img1, true)
    self:_check_image(img2, true)
-
    -- This function assumes the reference
    -- image is checked in the parent function
-
    return self[img2].R - self[img1].R
-
 end
 
 --- Calculate the tangent of a given image
@@ -175,108 +173,111 @@ end
 -- @return tangent force
 function NEB:tangent(image)
    self:_check_image(image)
-
    -- Determine energies of relevant images
    local E_prev = self[image-1].E
    local E_this = self[image].E
    local E_next = self[image+1].E
-
    -- Determine position differences
    local dR_prev = self:dR(image-1, image)
    local dR_next = self:dR(image, image+1)
-
+   local dR_this = self:dR(image, image)
    -- Returned value
    local tangent
-
    -- Determine relevant energy scenario
+   --if dR_next:norm(0) == 0.0 or dR_prev:norm(0)==0.0 or dR_this:norm(0)==0.0  then
+   --   tangent = dR_this
+   --   return tangent
    if E_next > E_this and E_this > E_prev then
-      
       tangent = dR_next
-
-   elseif E_next < E_this and E_this < E_prev then
-      
+      if dR_next:norm(0) == 0.0  then
+        return tangent
+      else
+        return tangent / tangent:norm(0)
+      end
+   elseif E_next < E_this and E_this < E_prev then      
       tangent = dR_prev
-
-   else
-      
+      if dR_prev:norm(0)==0.0 then
+        return tangent
+      else
+        return tangent / tangent:norm(0)
+      end   
+   else      
       -- We are at extremum, so mix
       local dEmax = m.max( m.abs(E_next - E_this), m.abs(E_prev - E_this) )
-      local dEmin = m.min( m.abs(E_next - E_this), m.abs(E_prev - E_this) )
-      
+      local dEmin = m.min( m.abs(E_next - E_this), m.abs(E_prev - E_this) )      
       if E_next > E_prev then
-	 tangent = dR_next * dEmax + dR_prev * dEmin
+         tangent = dR_next * dEmax + dR_prev * dEmin
+         if dR_next:norm(0) == 0.0 or dR_prev:norm(0)==0.0 then
+             return tangent
+         else
+         return tangent / tangent:norm(0)
+         end
       else
-	 tangent = dR_next * dEmin + dR_prev * dEmax
-      end
-      
+	       tangent = dR_next * dEmin + dR_prev * dEmax
+         if dR_next:norm(0) == 0.0 or dR_prev:norm(0)==0.0 then
+             return tangent
+         else
+             return tangent / tangent:norm(0)
+         end      
+      end      
    end
-
    -- At this point we have a tangent,
    -- now normalize and return it
-   return tangent / tangent:norm(0)
-
+   
+      --return tangent / tangent:norm(0)
+   --end
 end
-
-
 --- Determine whether the queried image is climbing
 -- @int image image queried
 -- @return true if the image is climbing
 function NEB:climbing(image)
-   self:_check_image(image)
-   
+   self:_check_image(image)   
    -- Determine energies of relevant images
    local E_prev = self[image-1].E
    local E_this = self[image  ].E
    local E_next = self[image+1].E
-
    -- Assert the tolerance is taken into consideration
    return (E_this - E_prev > self.climbing_tol) and
-       (E_this - E_next > self.climbing_tol)
-   
+       (E_this - E_next > self.climbing_tol)   
 end
-
 --- Calculate the spring force of a given image
 -- @int image the image to calculate the spring force of
 -- @return spring force
 function NEB:spring_force(image)
    self:_check_image(image)
-   
    -- Determine position norms
    local dR_prev = self:dR(image-1, image):norm(0)
-   local dR_next = self:dR(image, image+1):norm(0)
-   
+   local dR_next = self:dR(image, image+1):norm(0)   
    -- Set spring force as F = k (R1-R2) * tangent
-   return self.k[image] * (dR_next - dR_prev) * self:tangent(image)
-   
+   --if dR_prev==0.0 or dR_next==0.0 then
+   --  return self:tangent(image) --self.k[image] * (dR_next - dR_prev) * self:tangent(image)
+   --else
+    return self.k[image] * (dR_next - dR_prev) * self:tangent(image)  
+   --end
 end
-
-
 --- Calculate the perpendicular force of a given image
 -- @int image the image to calculate the perpendicular force of
 -- @return perpendicular force
 function NEB:perpendicular_force(image)
    self:_check_image(image)
-
+   if self:tangent(image):norm(0)==0.0 then
+     return self[image].F
+   else
    -- Subtract the force projected onto the tangent to get the perpendicular force
    local P = self[image].F:project(self:tangent(image))
-   return self[image].F - P
-
+   return self[image].F - P --self:tangent(image) 
+   end
 end
-
 --- Calculate the curvature of the force with regards to the tangent
 -- @int image the image to calculate the curvature of
 -- @return curvature
 function NEB:curvature(image)
    self:_check_image(image)
-
    local tangent = self:tangent(image)
-
    -- Return the scalar projection of F onto the tangent (in this case the
    -- tangent is already normalized so no need to no a normalization)
-   return self[image].F:flatdot(tangent)
-   
+   return self[image].F:flatdot(tangent)   
 end
-
 --- Calculate the resulting NEB force of a given image
 -- @int image the image to calculate the NEB force of
 -- @return NEB force
@@ -293,16 +294,18 @@ function NEB:neb_force(image)
        NEB_F = F - 2 * F:project( self:tangent(image) )
      else
        DNEB_F = 0.0
-       --DNEB_F=self:perpendicular_spring_force(image)-self:perpendicular_spring_force(image):project(self:perpendicular_force(image))*(self:perpendicular_force(image))
-       NEB_F = self:perpendicular_force(image) + self:spring_force(image) --+ DNEB_F
-       --print (DNEB_F)
+       NEB_F = self:perpendicular_force(image) + self:spring_force(image) + DNEB_F
      end
      return NEB_F
    end
    if self.neb_type == "DNEB" then
      if self.niter > self._climbing and self:climbing(image) then
        local F = self[image].F
-       NEB_F = F - 2 * F:project( self:tangent(image) )
+       if self:tangent(image):norm(0)==0.0 then
+           NEB_F = F
+       else 
+           NEB_F = F - 2 * F:project( self:tangent(image) )
+       end
      else
        DNEB_F = self:perpendicular_spring_force(image)-self:perpendicular_spring_force(image):project(self:perpendicular_force(image))*(self:perpendicular_force(image))
        NEB_F = self:perpendicular_force(image) + self:spring_force(image) + DNEB_F--+ self:perpendicular_spring_force(image)-self:perpendicular_spring_force(image):project(self:perpendicular_force(image))*(self:perpendicular_force(image))  --+DNEB_F 
@@ -310,7 +313,6 @@ function NEB:neb_force(image)
      end
      return NEB_F 
    end
-   --return NEB_F
 end
 
 --- Query the current coordinates of an image
@@ -318,96 +320,72 @@ end
 -- @return coordinates
 function NEB:R(image)
    self:_check_image(image, true)
-
    return NEB[image].R
 end
-
 --- Query the current force (same as `NEB:force` but with IO included)
 -- @int image the image
 -- @return force
 function NEB:force(image, IO)
-   self:_check_image(image)
-   --self.neb_type="NEB"
-   
+   self:_check_image(image)   
    if image == 1 then
       -- Increment step-counter
       self.niter = self.niter + 1
    end
-
    local F = self[image].F
    local tangent = self:tangent(image)
    local perp_F = self:perpendicular_force(image)
    local spring_F = self:spring_force(image)
-   --local NEB_F = self:neb_force(image)
    local NEB_F = self:neb_force(image)
-   --local DNEB_F = self:neb_force(image).DNEB_F
-   -- Things I want to output in files as control (all in 3xN format)
+    -- Things I want to output in files as control (all in 3xN format)
    if IO then
-
       local f
-
       -- Current coordinates (ie .R)
       f = io.open( ("NEB.%d.R"):format(image), "a")
       self[image].R:savetxt(f)
       f:close()
-
       -- Forces before (ie .F)
       f = io.open( ("NEB.%d.F"):format(image), "a")
       F:savetxt(f)
       f:close()
-
       -- Perpendicular force
       f = io.open( ("NEB.%d.F.P"):format(image), "a")
       perp_F:savetxt(f)
-      f:close()
-      
+      f:close()      
       -- Spring force
       f = io.open( ("NEB.%d.F.S"):format(image), "a")
       spring_F:savetxt(f)
       f:close()
-
       -- NEB Force
       f = io.open( ("NEB.%d.F.NEB"):format(image), "a")
       NEB_F:savetxt(f)
       f:close()
-
       -- Tangent
       f = io.open( ("NEB.%d.T"):format(image), "a")
       tangent:savetxt(f)
       f:close()
-
       -- dR - previous reaction coordinate
       f = io.open( ("NEB.%d.dR_prev"):format(image), "a")
       self:dR(image-1, image):savetxt(f)
       f:close()
-
       -- dR - next reaction coordinate
       f = io.open( ("NEB.%d.dR_next"):format(image), "a")
       self:dR(image, image+1):savetxt(f)
       f:close()
-
    end
-
    -- Fake return to test
-   return NEB_F
-   
+   return NEB_F   
 end
-
 --- Store the current step of the NEB iteration with the appropriate results
 function NEB:save(IO)
-
    -- If we should not do IO, return immediately
    if not IO then
       return
    end
-
    -- local E0
    local E0 = self[0].E
-
    -- Now setup the matrix to write the NEB-results
    local dat = array.Array( self.n_images + 2, 6)
    for i = 0, self.n_images + 1 do
-
       local row = dat[i+1]
       -- image number (0 for initial, n_images + 1 for final)
       row[1] = i
@@ -433,18 +411,14 @@ function NEB:save(IO)
       else
 	 row[6] = self:neb_force(i):norm():max()
       end
-
    end
-
    local f = io.open("NEB.results", "a")
    dat:savetxt(f)
    f:close()
-
 end
 
 --- Initialize all files that will be written to
-function NEB:init_files()
-   
+function NEB:init_files()   
    -- We clean all image data for a new run
    local function new_file(fname, ...)
       local f = io.open(fname, 'w')
@@ -454,10 +428,8 @@ function NEB:init_files()
       end
       f:close()
    end
-
    new_file("NEB.results", "NEB results file",
-	    "Image reaction-coordinate Energy E-diff Curvature F-max(atom)")
-   
+	    "Image reaction-coordinate Energy E-diff Curvature F-max(atom)")   
    for img = 1, self.n_images do
       new_file( ("NEB.%d.R"):format(img), "Coordinates")
       new_file( ("NEB.%d.F"):format(img), "Constrained force")
@@ -468,10 +440,7 @@ function NEB:init_files()
       new_file( ("NEB.%d.dR_prev"):format(img), "Reaction distance (previous)")
       new_file( ("NEB.%d.dR_next"):format(img), "Reaction distance (next)")
    end
-
 end
-
-
 --- Print to screen some information regarding the NEB algorithm
 function NEB:info()
   if self.neb_type=="NEB" then
@@ -483,7 +452,6 @@ function NEB:info()
    print ("  The NEB type is : D-Nudged Elastic Band   ")
    print ("============================================") 
   end
-
    print("NEB has " .. self.n_images)
    print("NEB uses climbing after " .. self._climbing .. " steps")
    local tmp = array.Array( self.n_images + 1 )
@@ -499,17 +467,17 @@ function NEB:info()
    end
    print("NEB spring constant: ")
    print(tostring(tmp))
-
 end
-
-
 -- Calculatin Perpendicular Spring force
 function NEB:perpendicular_spring_force(image)
   self:_check_image(image)
+  if self:tangent(image):norm(0)==0.0 then
+     return  self:spring_force(image)
+  else
   local PS=self:spring_force(image):project(self:tangent(image))
-  return self:spring_force(image)-PS
+     return self:spring_force(image)-PS
+  end
 end
-
 function NEB:file_exists(name)--name
    --local name
    --DM_name=tostring(name)
@@ -530,7 +498,5 @@ function NEB:file_exists(name)--name
    end
    --return check
 end
-
-
 
 return NEB
